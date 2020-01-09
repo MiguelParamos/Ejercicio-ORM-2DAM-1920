d6a0a2ccafadaef51022b1e822da2084c3777d17
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2020-01-09 10:07:55 +0100

    Commit inicial
    Solo abre una ventana principal inicial.

---
+++ 2dam.ORM/src/main/java/interfaces/VentanaPrincipal.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame {
+
+	public VentanaPrincipal() {
+		this.setSize(500,500);
+		this.setTitle("Tienda Cenec");
+		this.setVisible(true);
+	}
+	
+}
---
+++ 2dam.ORM/src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import interfaces.VentanaPrincipal;
+
+public final class Main {
+
+	public static void main(String[] args) {
+		VentanaPrincipal ventanaPrincipal=new VentanaPrincipal();
+	}
+
+}

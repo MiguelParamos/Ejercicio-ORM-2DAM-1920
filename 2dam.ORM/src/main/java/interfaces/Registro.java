67fbbcb55d1fce75f7a7b7729511fb7fc02e729d
idvera77 <48243491+idvera77@users.noreply.github.com>
2020-01-09 20:04:28 +0100

    Creacion clase Registro
    Diseño de la clase registro terminada, falta control de errores y creacion de usuario con los datos obtenidos.

---
+++ 2dam.ORM/src/main/java/interfaces/Registro.java
@@ -0,0 +1,129 @@
+package interfaces;
+/**
+ * @author Ivan Diaz y Jose Maria
+ */
+
+import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import main.Usuario;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Registro extends JPanel {
+	private JPasswordField textFieldRepetirPassword;
+	private JTextField textFieldRepetirEmail;
+	public Registro() {
+		setBackground(Color.decode("#5b9dc3"));
+		setSize(500,500);
+		setLayout(null);
+		
+		//JLabel utilizados como etiquetas.
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(124, 109, 72, 19);
+		lblNombre.setFont(new Font("Dialog", Font.BOLD, 16));
+		add(lblNombre);
+		
+		JLabel lblPassword = new JLabel("Contrase\u00F1a:");
+		lblPassword.setBounds(96, 136, 93, 19);
+		lblPassword.setFont(new Font("Dialog", Font.BOLD, 16));
+		add(lblPassword);
+		
+		JLabel lblRepetirPassword = new JLabel("Repetir Contrase\u00F1a:");
+		lblRepetirPassword.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblRepetirPassword.setBounds(37, 163, 154, 19);
+		add(lblRepetirPassword);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(143, 189, 54, 19);
+		lblEmail.setFont(new Font("Dialog", Font.BOLD, 16));
+		add(lblEmail);
+		
+		JLabel lblRepetirEmail = new JLabel("Repetir Email:");
+		lblRepetirEmail.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblRepetirEmail.setBounds(85, 217, 109, 19);
+		add(lblRepetirEmail);
+		
+		//TextField para rellenar los campos de Nombre, Password y Email.
+		final JTextField textFieldNombre = new JTextField();
+		textFieldNombre.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldNombre.setBounds(199, 111, 154, 19);
+		add(textFieldNombre);
+		
+		final JPasswordField textFieldPassword = new JPasswordField();
+		textFieldPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldPassword.setEchoChar('*');
+		textFieldPassword.setBounds(199, 138, 154, 19);
+		add(textFieldPassword);
+		
+		JPasswordField textFieldRepetirPassword = new JPasswordField();
+		textFieldRepetirPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldRepetirPassword.setEchoChar('*');
+		textFieldRepetirPassword.setBounds(199, 165, 154, 19);
+		add(textFieldRepetirPassword);
+
+		final JTextField textFieldEmail = new JTextField();
+		textFieldEmail.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldEmail.setBounds(199, 191, 154, 19);
+		add(textFieldEmail);
+		
+		final JTextField textFieldRepetirEmail = new JTextField();
+		textFieldRepetirEmail.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldRepetirEmail.setBounds(199, 218, 154, 19);
+		add(textFieldRepetirEmail);
+		
+		//Checkbox para indicar si el usuario se registra como tienda o no.
+		final JCheckBox checkboxTienda = new JCheckBox("\u00BFTienda?");
+		checkboxTienda.setHorizontalAlignment(SwingConstants.CENTER);
+		checkboxTienda.setBounds(201, 255, 116, 21);
+		checkboxTienda.setFont(new Font("Dialog", Font.BOLD, 12));
+		add(checkboxTienda);
+		
+		//Boton de registrar nuevo usuario.
+		JButton btnRegistrar = new JButton("Registro");
+		btnRegistrar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnRegistrar.setBounds(173, 303, 166, 28);
+		add(btnRegistrar);
+		
+		//Boton para ir a la pagina principal.
+		JButton btnAtras = new JButton("Atras");
+		btnAtras.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnAtras.setBounds(173, 342, 166, 28);
+		add(btnAtras);
+		
+		//Evento de botones
+		//Boton Registrar
+		btnRegistrar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(textFieldNombre.getText().isEmpty()) {
+					textFieldNombre.setBackground(Color.RED);
+				}
+				
+				String usuario = textFieldNombre.getText();
+				String password = String.valueOf(textFieldPassword.getPassword());
+				String email = textFieldEmail.getText();
+				if(checkboxTienda.isSelected()) {
+					
+				}
+				//Registro ventana = new Registro();
+				//getContentPane().add(ventana);
+				//Usuario nuevoUsuario = new Usuario(usuario, password, email, null, null, null);
+			}
+		});
+		
+		//Boton Atras
+		btnAtras.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+			}
+		});
+		
+		
+		
+	}
+}

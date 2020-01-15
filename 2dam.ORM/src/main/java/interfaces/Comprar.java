ff88fa643c9cf6c37096d2afa53e7ffe7235ea0b
javidpozo14 <50463457+javidpozo14@users.noreply.github.com>
2020-01-15 20:37:57 +0100

    Update Comprar.java
    Javi- clase Comprar

--- 2dam.ORM/src/main/java/interfaces/Comprar.java
+++ 2dam.ORM/src/main/java/interfaces/Comprar.java
@@ -1,9 +1,21 @@
 package interfaces;
 
 import javax.swing.JPanel;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Comprar extends JPanel {
 	public Comprar() {
+		setLayout(null);
+		
+		JList list = new JList();
+		list.setBounds(10, 36, 419, 253);
+		add(list);
+		
+		JLabel lblComprar = new JLabel("COMPRAR");
+		lblComprar.setFont(new Font("Yu Gothic", Font.BOLD, 14));
+		lblComprar.setBounds(10, 11, 117, 23);
+		add(lblComprar);
 	}
-
 }

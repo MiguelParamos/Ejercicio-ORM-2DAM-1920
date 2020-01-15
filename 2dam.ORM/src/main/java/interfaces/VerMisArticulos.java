59d96996af8f0920b027164639fb9dc62bb53790
Javier Rodriguez <javirodriguez7@hotmail.es>
2020-01-15 20:49:49 +0100

    Clase VerMisArticulos
    He agregado  la función de rellenar la lista con los articulos de los usuarios y documentación de la clase.

--- 2dam.ORM/src/main/java/interfaces/VerMisArticulos.java
+++ 2dam.ORM/src/main/java/interfaces/VerMisArticulos.java
@@ -1,49 +1,53 @@
 package interfaces;
-
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
+
+import main.Articulo;
+import main.Usuario;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.JList;
 import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+/**
+ * Esta clase modela los articulos que ha comprado un usuario.
+ * @author Javier Rodríguez y Juan Carlos
+ *
+ */
 public class VerMisArticulos extends JPanel{
-	public VerMisArticulos() {
+	private VentanaPrincipal ventanaPrincipal;
+	private Usuario user;
+	@SuppressWarnings("unchecked")
+	/**
+	 * El constructor principal de la clase.
+	 * @param vp la clase VentanaPrincipal del programa.
+	 * @param us la clase Usuario del programa.
+	 */
+	public VerMisArticulos(VentanaPrincipal vp,Usuario us) {
+		this.ventanaPrincipal=vp;
+		this.user=us;
 		setSize(new Dimension(500, 500));
 		setLayout(null);
 		
 		//LISTA DE ARTICULOS
-		JList listaArticulos = new JList();
+		//Foreach para rellenar los articulos que dependan del usuario.
+		DefaultListModel dLM=new DefaultListModel<String>();
+		for(Articulo a: user.getArticulosComprados()) {
+			dLM.addElement(a.getArtName());
+		}
+		JList listaArticulos = new JList(dLM);
 		listaArticulos.setBounds(135, 54, 233, 282);
-		listaArticulos.setModel(new AbstractListModel() {
-			String[] values = new String[] {"Articulo1", "Articulo2", "Articulo3"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
 		add(listaArticulos);
-		
-		//BOTÓN VER ARTICULOS
-		JButton btnVerArticulos = new JButton("Ver art\u00EDculos");
-		btnVerArticulos.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		btnVerArticulos.setBounds(201, 347, 108, 23);
-		add(btnVerArticulos);
 		
 		//BOTÓN ATRÁS
 		JButton btnAtras = new JButton("Atr\u00E1s");
 		btnAtras.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				//Boton atrás pendiente para hacer más adelante.
 			}
 		});
 		btnAtras.setBounds(49, 425, 89, 23);

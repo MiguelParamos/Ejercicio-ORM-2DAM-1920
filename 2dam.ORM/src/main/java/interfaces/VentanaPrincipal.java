85c5db0207a8f62a5405e0c52d8b949f04c0c7fa
kareka78 <47817807+kareka78@users.noreply.github.com>
2020-01-16 09:28:18 +0100

    Ultimos retoques

--- 2dam.ORM/src/main/java/interfaces/VentanaPrincipal.java
+++ 2dam.ORM/src/main/java/interfaces/VentanaPrincipal.java
@@ -4,20 +4,20 @@ import javax.swing.JFrame;
 
 public class VentanaPrincipal extends JFrame {
     private Registro pantallaRegistro;
-    //private PantallaInicial pantallaInicial;
+    private PantallaInicial pantallaInicial;
 	public VentanaPrincipal() {
 		this.setSize(500,500);
 		this.setTitle("Tienda Cenec");
 		this.setVisible(true);
 	}
-	  /*public void irPantallaInicial(){
+	  public void irPantallaInicial(){
 		   if(this.pantallaInicial==null) {
-			   this.pantallaInicial=new PantallaInicial(this);
+			   this.pantallaInicial=new PantallaInicial();
 		   }
 		   this.setTitle("pantallaInicial");
 		   this.pantallaRegistro.setVisible(false);
 		   this.setContentPane(this.pantallaInicial);
 		   this.pantallaInicial.setVisible(true);
 		   
-	   }*/
+	   }
 }

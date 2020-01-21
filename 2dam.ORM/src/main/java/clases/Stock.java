7e65763c49284e3468f6158458a3dbd9eadb31b0
Malkien <kevin1cuenca@gmail.com>
2020-01-21 19:55:33 +0100

    Update de la clase Stock
    Añadida la funcion añadirStock y setStock

--- 2dam.ORM/src/main/java/clases/Stock.java
+++ 2dam.ORM/src/main/java/clases/Stock.java
@@ -42,12 +42,13 @@ public class Stock {
 						articulosBD.getFloat("precio"),
 						articulosBD.getString("descripcion")
 						);
-				ResultSet stockBD = statement.executeQuery("SELECT cantidad FROM Stock");
+				ResultSet stockBD = statement.executeQuery("SELECT cantidad FROM Stock WHERE nombre_Articulo="+articulo.getArtName());
 				stockBD.next();//OBVIO QUE SOLO HAY UNO
 				
 				HashMap.put(articulo, stockBD.getShort("cantidad"));//AÑADO AL HASHMAP EL ARTICULO Y LA CANTIDAD
 				stockBD.close();
 			}
+			
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -64,20 +65,53 @@ public class Stock {
 	}
 	
 	/**
-	 * Funcion para añadir stock a la bbdd
+	 * Funcion que añade la cantidad que le damos más la que tiene un articulo en el stock tanto en la bbdd como en el programa
 	 * @author Malkien
 	 * @param articulo el articulo al que le vas a añadir el stock
 	 * @param cantidad la cantidad que le vas a añadir
 	 * @throws StockNoModificadoException 
 	 */
-	public void AñadirStock(Articulo articulo,short cantidad) throws StockNoModificadoException {
-		try {
-			
-			this.HashMap.replace(articulo,(short) (this.HashMap.get(articulo) + cantidad));
-		}catch(Exception e) {//Excepcion personalizada
-			throw new StockNoModificadoException();
-		}
+	public void añadirStock(Articulo articulo,short cantidad) throws StockNoModificadoException {
 		
+		Short total = (short) (this.HashMap.get(articulo)+cantidad);//Calcula la nueva cantidad que habra en el stock
+		modificarStock(articulo,total);//Le pasamos la suma de la cantidad que tiene con la que le añadimos
+		
+	}
+	/**
+	 * Funcion que machaca la cantidad que tiene un articulo en el stock tanto en la bbdd como en el programa
+	 * @param articulo el articulo al que vas a modificar el stock
+	 * @param cantidad la cantidad que le queremos poner
+	 * @throws StockNoModificadoException
+	 */
+	public void setStock(Articulo articulo, Short cantidad) throws StockNoModificadoException {
+		modificarStock(articulo,cantidad);//Le pasamos la cantidad que tendrá ahora
+	}
+	/**
+	 * Funcion para modificar el stock a la bbdd y al HashMap
+	 * @author Malkien
+	 * @param articulo el articulo al que le vas a modificar el stock
+	 * @param cantidad la cantidad que tendra ahora
+	 * @throws StockNoModificadoException 
+	 */
+	private void modificarStock(Articulo articulo, Short cantidad) throws StockNoModificadoException {
+		Connection conexion = null;
+		try {
+			conexion = DriverManager.getConnection(baseDatos,usuario,contraseña);
+			Statement modificarEnBD = conexion.createStatement();
+			//Actualiza el stock en la bbdd
+			modificarEnBD.executeUpdate("Update Stock SET cantidad="+cantidad+" WHERE nombre_Articulo="+articulo.getArtName());
+			//Actualiza el stock en el HashMap
+			this.HashMap.replace(articulo, cantidad);
+		}catch(Exception e) {
+			throw new StockNoModificadoException();//Excepcion personalizada
+		}finally {
+			try {
+				conexion.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	

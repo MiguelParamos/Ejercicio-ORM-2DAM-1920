930520aa5164c4eee3e4bb229db9602fcb624e63
Silver (Ale) <3512126@gmail.com>
2020-01-21 20:55:07 +0100

    Añadido documentación de las excepciones y quitado el "super" del constructor de las mismas
    Solucionado pequeño error de las excepciones que requería que le pasases un parámetro de tipo String por constructor.
    
    - Se ha añadido la documentación de las excepciones de login y registro.

--- 2dam.ORM/src/main/java/excepciones/LoginIncorrectoException.java
+++ 2dam.ORM/src/main/java/excepciones/LoginIncorrectoException.java
@@ -1,8 +1,14 @@
 package excepciones;
 
+/***
+ * Esta excepción se usa cuando el login (que hace conexión con la base de datos en la función DAO) falla.
+ * @author Silver (Ale)
+ * @author Pablo
+ *
+ */
 public class LoginIncorrectoException extends Exception{
 
-	public LoginIncorrectoException(String msg) {
+	public LoginIncorrectoException() {
 		super("Los datos introducidos son incorrectos. Prueba a volver a introducirlos.");
 	}
 }
--- 2dam.ORM/src/main/java/excepciones/RegistroIncorrectoException.java
+++ 2dam.ORM/src/main/java/excepciones/RegistroIncorrectoException.java
@@ -1,8 +1,14 @@
 package excepciones;
 
+/***
+ * Esta excepción se usa cuando el registro (que hace conexión con la base de datos en la función DAO) falla.
+ * @author Silver (Ale)
+ * @author Pablo
+ *
+ */
 public class RegistroIncorrectoException extends Exception{
 
-	public RegistroIncorrectoException(String msg) {
+	public RegistroIncorrectoException() {
 		super("Los datos introducidos para el registro son erróneos o ya coinciden con los ya existentes. Prueba a intentarlo de nuevo.");
 	}
 }

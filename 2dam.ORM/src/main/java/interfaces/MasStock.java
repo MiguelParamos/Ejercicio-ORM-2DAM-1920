6c57450116b80e8ccedf44538f787e96ec8785fa
amrMoreno <adrianmorenoruiz@gmail.com>
2020-01-22 13:13:41 +0100

    Pantalla MasStock
    Implementado la posibilidad de seleccionar un artículo, añadirle stock y registrarlo en base de datos.

--- 2dam.ORM/src/main/java/interfaces/MasStock.java
+++ 2dam.ORM/src/main/java/interfaces/MasStock.java
@@ -4,25 +4,71 @@ package interfaces;
  * @author Adrian
  */
 import javax.swing.JPanel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+import clases.Articulo;
+import clases.Stock;
 import clases.Usuario;
+import excepciones.StockNoModificadoException;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MasStock extends JPanel {
 	private JTextField campoNumericoMasStock;
 	private Usuario usuario;
 	private VentanaPrincipal ventana;
-	
+	private final String baseDatos = "jdbc:mysql://127.0.0.1:3306/nombredb";//LA BBDD
+	private final String usuarioBd = "2dam";//EL USUARIO
+	private final String contraseña = "2dam";//LA CONTRASEÑA
+	private HashMap<Articulo, Short> HashMap=new HashMap<Articulo, Short>();//El HashMap con los Articulos y su Stock
+	 private  Articulo articulo ;
 	/**
 	 * Create the panel.
 	 */
 	public MasStock(VentanaPrincipal v) {
 		setLayout(null);
+		Connection conexion = null;
 		
+		try {
+			/**
+			 * Conexión a la bd 
+			 */
+			conexion = DriverManager.getConnection(baseDatos,usuarioBd,contraseña);
+			Statement statement = conexion.createStatement();
+			ResultSet articulosBD=statement.executeQuery("SELECT * FROM Articulo");
+			while(articulosBD.next()) {
+				
+				articulo= new Articulo(
+						articulosBD.getString("nombre"),
+						articulosBD.getFloat("precio"),
+						articulosBD.getString("descripcion")
+						);
+				ResultSet stockBD = statement.executeQuery("SELECT cantidad FROM Stock WHERE nombre_Articulo="+articulo.getArtName());
+				stockBD.next();
+				
+				// Se añade al 'HasMap' el artículo y la cantidad del stock 
+				HashMap.put(articulo, stockBD.getShort("cantidad"));
+				stockBD.close();
+			}
+
 		//Boton atras
 		JButton btnAtrasMasStock = new JButton("Atras");
+		btnAtrasMasStock.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ventana.setVisible(true);
+			}
+		});
 		btnAtrasMasStock.setBounds(332, 252, 89, 23);
 		add(btnAtrasMasStock);
 		
@@ -30,8 +76,7 @@ public class MasStock extends JPanel {
 		JLabel lblAñadirStock = new JLabel("A\u00F1adir Stock");
 		lblAñadirStock.setBounds(29, 256, 70, 14);
 		add(lblAñadirStock);
-		
-		
+
 		//JTextField "campoNumericoMasStock"
 		campoNumericoMasStock = new JTextField();
 		campoNumericoMasStock.setToolTipText("");
@@ -39,15 +84,40 @@ public class MasStock extends JPanel {
 		add(campoNumericoMasStock);
 		campoNumericoMasStock.setColumns(10);
 		
-		
+		String n = campoNumericoMasStock.getText();
+		int CantidadNumero=Integer.parseInt(n);
+		 final short campoNumeroStock =(short)CantidadNumero;
+		 
 		//Boton  añadir stock a la lista
 		JButton btnAñadirMasStock = new JButton("A\u00F1adir");
-		btnAñadirMasStock.setBounds(233, 252, 89, 23);
+		btnAñadirMasStock.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Stock s=new Stock();		
+				try {
+					s.añadirStock(articulo,campoNumeroStock);
+				} catch (StockNoModificadoException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		});
+		
+		btnAñadirMasStock.setBounds(237, 252, 89, 23);
 		add(btnAñadirMasStock);
 		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				conexion.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
-		//Select sin uso.
-		String selectArticulo = "SELECT * FROM Articulo";
-		
+	
 	}
 }

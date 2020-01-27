2cefae04ca78b2b9a793d600be6b90b0400cde6d
SaraPdrui <SaraPdrui@outlook.com>
2020-01-27 11:07:42 +0100

    Pantalla Comprar - Primer Punto (No definitivo)

--- 2dam.ORM/src/main/java/interfaces/Comprar.java
+++ 2dam.ORM/src/main/java/interfaces/Comprar.java
@@ -4,11 +4,8 @@ package interfaces;
  * @author Alberto Riera
  */
 import javax.swing.JPanel;
-import javax.swing.JTextPane;
-
 import clases.Articulo;
 import clases.Usuario;
-import classes.Argument;
 import componentes.BotonMenu;
 
 import javax.swing.JList;
@@ -19,18 +16,20 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.ListModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.JTextField;
+import javax.swing.JSpinner;
 
 public class Comprar extends JPanel {
 
-	private Usuario user;
+	private Usuario user;//Usuario que se pasa por el constructor
 	private VentanaPrincipal ventana;
-	private JList listaComprar; 
-	private JTextField textField;
-
-	
+	private JList listaComprar; //Jlist que muestra los artículos en stock
+	private JTextField saldo; //JTextField que muestra el saldo del que dispone el Usuario
+	private JLabel lblComprar;//Label que muestra el texto principal de la pantalla
+	private BotonMenu botonComprar; //Boton que lleva al evento que realiza la compra de un articulo
+	private JSpinner cantidadSpinner;// Spinner que muestra la cantidad de articulos que el usuario puede comprar
+	private SpinnerNumberModel m_numberSpinnerModel;//Numeros maximos y mínimos que se mostraran en el Spinner
 
 	public Comprar(Usuario user, VentanaPrincipal ventana) {
 		
@@ -39,28 +38,20 @@ public class Comprar extends JPanel {
 
 		setLayout(null);
 
+		
+		//-----------------------LABEL CON EL TÍTULO DE LA PANTALLA---------------------------------
+		
+		this.lblComprar = new JLabel("COMPRAR");
+		lblComprar.setFont(new Font("Yu Gothic", Font.BOLD, 14));
+		lblComprar.setBounds(10, 11, 117, 23);
+		add(lblComprar);		
+		
+		//-----------------------JLIST EN LA QUE SE AÑADEN LOS ARTÍCULOS-----------------------------
+		
 		DefaultListModel model = new DefaultListModel();
 		this.listaComprar = new JList(model);
 		listaComprar.setBounds(10, 47, 99, 185);
 		add(listaComprar);
-
-		JLabel lblComprar = new JLabel("COMPRAR");
-		lblComprar.setFont(new Font("Yu Gothic", Font.BOLD, 14));
-		lblComprar.setBounds(10, 11, 117, 23);
-		add(lblComprar);
-
-		BotonMenu comprar = new BotonMenu("Comprar");
-		comprar.setBounds(144, 239, 172, 50);
-		add(comprar);
-		
-		JList list = new JList();
-		list.setBounds(141, 114, 53, -66);
-		add(list);
-		
-		textField = new JTextField();
-		textField.setBounds(124, 45, 86, 20);
-		add(textField);
-		textField.setColumns(10);
 		
 		ArrayList<Articulo> articulo = user.getArticulosComprados();
 		
@@ -69,11 +60,45 @@ public class Comprar extends JPanel {
 			model.add(i, articuloMostrado.getArtName());
 		}
 
+
+		//-----------------------BOTON QUE REALIZA LA COMPRA-------------------------------------
+
+		this.botonComprar = new BotonMenu("Comprar");
+		botonComprar.setBounds(144, 239, 172, 50);
+		add(botonComprar);
+		
+		//-----------------------JTEXTFIEL QUE MUESTRA EL SALDO-------------------------------------
+		
+		saldo = new JTextField();
+		saldo.setBounds(124, 45, 86, 20);
+		add(saldo);
+		saldo.setColumns(10);
+		saldo.setText(String.valueOf(user.getSaldo()));
+		
+		//-----------------------SPINNER QUE MUESTRA LA CANTIDAD QUE SE PUEDE COMPRAR-------------------------------------
+		
+		int current = new Integer(1);
+		int min = new Integer(1);
+		int max = new Integer(articulo.size());
+		int step = new Integer(1);
+	    this.m_numberSpinnerModel = new SpinnerNumberModel(current, min, max, step);
+	   
+		this.cantidadSpinner = new JSpinner(m_numberSpinnerModel);
+		cantidadSpinner.setBounds(124, 90, 39, 23);
+		
+		add(cantidadSpinner);
+		
+		
+		
+		
+		
 		// ------------------ EVENTOS DE LOS BOTONES ------------------
 
-		comprar.addMouseListener(new MouseAdapter() {
+		botonComprar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				
+				user.
 
 			}
 		});
